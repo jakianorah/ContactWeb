88be0841eac27d7a4c8347a271a7bded24d686c9
Jakia Norah <jakianorah@gmail.com>
2017-09-30 11:00:03 -0500

    changed property to copy always for jquery

--- ContactWeb/App_Start/BundleConfig.cs
+++ ContactWeb/App_Start/BundleConfig.cs
@@ -8,18 +8,18 @@ namespace ContactWeb
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/js/lib/jquery").Include(
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/js/lib/jquery.min.js"));
 
-            bundles.Add(new ScriptBundle("~/js/lib/bootstrap").Include(
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/js/lib/bootstrap.min.js"));
 
-            bundles.Add(new ScriptBundle("~/js/lib/datatables").Include(
+            bundles.Add(new ScriptBundle("~/bundles/datatables").Include(
                         "~/js/lib/jquery.dataTables.min.js",
                         "~/js/lib/dataTables.bootstrap.min.js",
                         "~/js/lib/dataTables.colReorder.min.js"));
 
-            bundles.Add(new ScriptBundle("~/js/lib/jqueryui").Include(
+            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
                         "~/js/lib/jquery-ui.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
